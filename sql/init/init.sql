5ebe7465ce3b6bcfcd0fc161dd01f4a7299fb444
kenya6111 <kenyanke6111@gmail.com>
2025-07-24 21:12:01 +0900

    init.sql修正

--- sql/init/init.sql
+++ sql/init/init.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
 );
 
-INSERT INTO users (name, email)
+INSERT INTO users (name, email,password)
 VALUES
-('Alice', 'alice@example.com'),
-('Bob', 'bob@example.com'),
-('Charlie', 'charlie@example.com');
+('Alice', 'alice@example.com', 'password'),
+('Bob', 'bob@example.com','password'),
+('Charlie', 'charlie@example.com','password');
