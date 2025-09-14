5c577beea439afb621e24110519dabc2ff1528eb
kenya6111 <kenyanke6111@gmail.com>
2025-09-15 02:06:31 +0900

    削除

--- application/user/errors.go
+++
@@ -1,8 +0,0 @@
-package usecase
-
-// import "errors"
-
-// // var ErrNoData = errors.New("get 0 record from db.Query")
-// // var ErrDuplicateData = errors.New("already exist user by email")
-// // var ErrMismatchData = errors.New("mismatch password and confirmPassword")
-// // var ErrNoRequestParam = errors.New("get no value from request")
