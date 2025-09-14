b506224a4ed3c647852197de7765dd0e2840476a
kenya6111 <kenyanke6111@gmail.com>
2025-09-15 02:59:53 +0900

    interfaceをuserServicerを削除

--- presentation/user/services/service.go
+++ presentation/user/services/service.go
@@ -1,12 +1,6 @@
 package services
 
-import (
-	"context"
-	domain "twitter-clone-go/domain/user"
-	"twitter-clone-go/request"
-)
-
-type UserServicer interface {
-	GetUserList() ([]domain.User, error)
-	SignUp(c context.Context, signUpInfo request.SignUpInfo) error
-}
+// type UserServicer interface {
+// 	GetUserList() ([]domain.User, error)
+// 	SignUp(c context.Context, signUpInfo request.SignUpInfo) error
+// }
