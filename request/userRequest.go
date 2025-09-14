8cd9ca8903cafda568067feaf08ac1852cb1be7c
kenya6111 <kenyanke6111@gmail.com>
2025-09-14 21:22:18 +0900

    nameプロパティ追加

--- request/userRequest.go
+++ request/userRequest.go
@@ -1,6 +1,7 @@
 package request
 
 type SignUpInfo struct {
+	Name            string `validate:"required"`
 	Email           string `validate:"required,email"`
 	Password        string `validate:"required,gte=8,has_kigou,has_han_su,has_lower_ei,has_upper_ei"`
 	ConfirmPassword string `validate:"required,gte=8,has_kigou,has_han_su,has_lower_ei,has_upper_ei"`
