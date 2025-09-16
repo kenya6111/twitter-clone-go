614ff15f2c650f11c2511835b5411905dad4c770
kenya6111 <kenyanke6111@gmail.com>
2025-09-16 23:40:05 +0900

    mainに統合

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"log"
-	"twitter-clone-go/api"
+	application "twitter-clone-go/application/user"
+	"twitter-clone-go/infrasctructure/email/mailcatcher"
 	"twitter-clone-go/infrasctructure/storage/postgres"
+	"twitter-clone-go/interface/http"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/memstore"
+	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
@@ -14,7 +19,22 @@ func main() {
 		log.Fatal(err)
 	}
 	defer pool.Close()
-	api.Run(pool)
+	router := gin.Default()
+
+	var emailService = mailcatcher.NewMainCatcherEmailService("test")
+	repo := postgres.NewUserRepository(pool)
+	tx := postgres.NewTransaction(pool)
+	dSer := application.NewUserDomainService(repo)
+	ser := application.NewUserUsecase(repo, tx, dSer, emailService)
+	con := http.NewUserHandler(ser)
+
+	store := memstore.NewStore([]byte("secret"))
+
+	router.Use(sessions.Sessions("mySession", store))
+	router.GET("/", con.Home)
+	router.GET("/users", con.GetUserListHandler)
+	router.POST("/signup", con.SignUpHandler)
+	router.GET("/health_check", con.HealthCheck)
 }
 
 func init() {
