58eec5abe4ad3d9febb914aba841af7577d15530
kenya6111 <kenyanke6111@gmail.com>
2025-09-16 23:56:38 +0900

    コメント

--- main.go
+++ main.go
@@ -14,22 +14,32 @@ import (
 )
 
 func main() {
+	// DB接続
 	pool, err := postgres.SetupDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer pool.Close()
-	router := gin.Default()
 
 	var emailService = mailcatcher.NewMainCatcherEmailService("test")
-	repo := postgres.NewUserRepository(pool)
+	// トランザクションの注入
 	tx := postgres.NewTransaction(pool)
+
+	// リポジトリの注入
+	repo := postgres.NewUserRepository(pool)
+
+	// サービスの注入
 	dSer := application.NewUserDomainService(repo)
+
+	// ユースケースの注入
 	ser := application.NewUserUsecase(repo, tx, dSer, emailService)
+
+	// ハンドラーの注入
 	con := http.NewUserHandler(ser)
 
 	store := memstore.NewStore([]byte("secret"))
 
+	router := gin.Default()
 	router.Use(sessions.Sessions("mySession", store))
 	router.GET("/", con.Home)
 	router.GET("/users", con.GetUserListHandler)
