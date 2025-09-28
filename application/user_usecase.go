723a2d38ef8b9967c08feb6f9a71fb1e147efe00
kenya6111 <kenyanke6111@gmail.com>
2025-09-28 15:03:28 +0900

    json追加

--- application/user_usecase.go
+++ application/user_usecase.go
@@ -5,31 +5,29 @@ import (
 	"twitter-clone-go/apperrors"
 	"twitter-clone-go/domain"
 	"twitter-clone-go/domain/service"
-	"twitter-clone-go/pkg/crypt"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 type SignUpInfo struct {
-	Name            string
-	Email           string
-	Password        string
-	ConfirmPassword string
+	Name            string `json:"name" binding:"required"`
+	Email           string `json:"email" binding:"required,email"`
+	Password        string `json:"password" binding:"required"`
+	ConfirmPassword string `json:"confirmPassword" binding:"required"`
 }
 
 type UserUsecaseImpl struct {
-	repo         domain.UserRepository
-	tx           domain.Transaction
-	dSer         domain.UserDomainService
-	emailService service.EmailService
+	repo           domain.UserRepository
+	tx             domain.Transaction
+	dSer           domain.UserDomainService
+	emailService   service.EmailService
+	passwordHasher service.PasswordHasher
 }
 type UserUsecase interface {
 	GetUserList() ([]domain.User, error)
 	SignUp(c context.Context, signUpInfo SignUpInfo) error
 }
 
-func NewUserUsecase(r domain.UserRepository, tx domain.Transaction, dSer domain.UserDomainService, emailService service.EmailService) *UserUsecaseImpl {
-	return &UserUsecaseImpl{repo: r, tx: tx, dSer: dSer, emailService: emailService}
+func NewUserUsecase(r domain.UserRepository, tx domain.Transaction, dSer domain.UserDomainService, emailService service.EmailService, passwordHasher service.PasswordHasher) *UserUsecaseImpl {
+	return &UserUsecaseImpl{repo: r, tx: tx, dSer: dSer, emailService: emailService, passwordHasher: passwordHasher}
 }
 
 func (u *UserUsecaseImpl) GetUserList() ([]domain.User, error) {
@@ -53,9 +51,9 @@ func (u *UserUsecaseImpl) SignUp(ctx context.Context, request SignUpInfo) error 
 		return err
 	}
 
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password.Value()), bcrypt.DefaultCost)
+	hash, err := u.passwordHasher.HashPassword(user.Password.Value())
 	if err != nil {
-		return apperrors.GetDataFailed.Wrap(err, "fail to generate has value from password")
+		return apperrors.GenerateHashFailed.Wrap(err, "fail to generate hash value from password")
 	}
 
 	err = u.tx.Do(ctx, func(ctx context.Context) error {
@@ -64,7 +62,10 @@ func (u *UserUsecaseImpl) SignUp(ctx context.Context, request SignUpInfo) error 
 			return apperrors.InsertDataFailed.Wrap(err, "fail to insert user ")
 		}
 
-		token, _ = crypt.GenerateSecureToken(32)
+		token, err = u.passwordHasher.GenerateSecureToken(32)
+		if err != nil {
+			return apperrors.GenerateTokenFailed.Wrap(err, "fail to generate secure token ")
+		}
 
 		_, err := u.repo.CreateEmailVerifyToken(ctx, createdUser.ID, token)
 		if err != nil {
@@ -76,8 +77,8 @@ func (u *UserUsecaseImpl) SignUp(ctx context.Context, request SignUpInfo) error 
 		return err
 	}
 
-	if err := u.emailService.SendInvitationEmail(token, user.Email); err != nil {
-		return apperrors.GenerateTokenFailed.Wrap(err, "fail to send invitation mail")
+	if err := u.emailService.SendInvitationEmail(user.Email, token); err != nil {
+		return apperrors.SendEmailFailed.Wrap(err, "fail to send invitation mail")
 	}
 
 	return nil
