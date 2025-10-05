f1ccf1361894e317966035157af09c5b418c60ac
kenya6111 <kenyanke6111@gmail.com>
2025-10-05 20:52:44 +0900

    default.sql修正

--- infrastructure/storage/postgres/testdata/email_verify_repository/default.sql
+++ infrastructure/storage/postgres/testdata/email_verify_repository/default.sql
@@ -1,3 +1,9 @@
+INSERT INTO users (id, name, email, password, is_active)
+VALUES
+  ('1', 'user1', 'user1@example.com', 'hashed_PW1!', TRUE),
+  ('2', 'user2', 'user2@example.com', 'hashed_PW2!', FALSE);
+
+
 INSERT INTO email_verify_token (user_id, token, expires_at, created_at)
 VALUES
   ('1', 'abc123xyztoken', NOW() + INTERVAL '24 hours', NOW()),
