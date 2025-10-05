3c3e653eef9bfba2c226a7153ef5f1fb88746005
kenya6111 <kenyanke6111@gmail.com>
2025-10-05 20:54:25 +0900

    reconstruct追加

--- domain/email_verify_token.go
+++ domain/email_verify_token.go
@@ -3,10 +3,12 @@ package domain
 import (
 	"context"
 	"time"
+
+	ulid "github.com/oklog/ulid/v2"
 )
 
 type EmailVerifyToken struct {
-	ID        int32
+	ID        string
 	UserID    string
 	Token     string
 	ExpiresAt time.Time
@@ -15,8 +17,18 @@ type EmailVerifyToken struct {
 
 type EmailVerifyTokenRepository interface {
 	Save(ctx context.Context, userId string, token string) (*EmailVerifyToken, error)
-	FindByToken(ctx context.Context, token string, expiredAt time.Time) (*EmailVerifyToken, error)
+	FindByToken(ctx context.Context, token string) (*EmailVerifyToken, error)
 	DeleteByToken(ctx context.Context, token string) error
+}
+
+func ReconstructEmailVerifyToken(id string, userId string, token string, expiredAt time.Time, createdAt time.Time) (*EmailVerifyToken, error) {
+	return &EmailVerifyToken{
+		ID:        ulid.Make().String(),
+		UserID:    userId,
+		Token:     token,
+		ExpiresAt: expiredAt,
+		CreatedAt: createdAt,
+	}, nil
 }
 
 func IsExpired(expiredAt time.Time) bool {
