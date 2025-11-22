3bde2bd3698531fba1c37dbc61ad41c3cf2ff8d4
kenya6111 <kenyanke6111@gmail.com>
2025-11-23 00:41:28 +0900

    errorコード分類

--- apperrors/errorcode.go
+++ apperrors/errorcode.go
@@ -3,25 +3,24 @@ package apperrors
 type ErrCode string
 
 const (
-	Unknown ErrCode = "U000"
-
-	InsertDataFailed    ErrCode = "S001"
-	GetDataFailed       ErrCode = "S002"
-	NAData              ErrCode = "S003"
-	NoTargetData        ErrCode = "S004"
-	UpdateDataFailed    ErrCode = "S005"
-	DuplicateData       ErrCode = "S006"
-	GenerateHashFailed  ErrCode = "S007"
-	GenerateTokenFailed ErrCode = "S008"
-	DeleteDataFailed    ErrCode = "S009"
-	SendMailFailed      ErrCode = "S010"
-	SessionSaveFailed   ErrCode = "S011"
-	SendEmailFailed     ErrCode = "S012"
-	Unauthorized        ErrCode = "S013"
-	LogoutFailed        ErrCode = "S014"
-
-	ReqBodyDecodeFailed ErrCode = "R001"
-	BadParam            ErrCode = "R002"
+	Unknown               ErrCode = "U000"
+	ReqBodyDecodeFailed   ErrCode = "R001"
+	ReqBadParam           ErrCode = "R002"
+	AuthUnauthorized      ErrCode = "A001"
+	AuthSessionSaveFailed ErrCode = "A002"
+	AuthLogoutFailed      ErrCode = "A003"
+	InsertDataFailed      ErrCode = "S001"
+	GetDataFailed         ErrCode = "S002"
+	NAData                ErrCode = "S003"
+	NoTargetData          ErrCode = "S004"
+	UpdateDataFailed      ErrCode = "S005"
+	DuplicateData         ErrCode = "S006"
+	DeleteDataFailed      ErrCode = "S007"
+	GenerateHashFailed    ErrCode = "T001"
+	GenerateTokenFailed   ErrCode = "T001"
+	SendMailFailed        ErrCode = "M001"
+	SendEmailFailed       ErrCode = "M001"
+	SaveLocalFileFailed   ErrCode = "F001"
 )
 
 func (code ErrCode) Wrap(err error, message string) error {
