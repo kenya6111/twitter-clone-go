25e2c0e30a7e8f78248d3ab4326a368df8f1435b
kenya6111 <kenyanke6111@gmail.com>
2025-11-24 21:12:21 +0900

    ddl追加

---
+++ db/migrations/000006_create_likes_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS likes (
+    tweet_id INTEGER NOT NULL,
+    user_id TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (tweet_id, user_id)
+    -- FOREIGN KEY (tweet_id) REFERENCES tweets(id) ON DELETE CASCADE,
+    -- FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
