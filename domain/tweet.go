15b01fba3a5d8cca3d4a903d95d3961c6653c0b7
kenya6111 <kenyanke6111@gmail.com>
2025-11-24 21:17:58 +0900

    不要な処理削除

--- domain/tweet.go
+++ domain/tweet.go
@@ -3,61 +3,37 @@ package domain
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"twitter-clone-go/apperrors"
 	"unicode/utf8"
-
-	ulid "github.com/oklog/ulid/v2"
 )
 
 type Tweet struct {
-	ID        string
+	ID        int
 	UserID    string
 	Content   string
-	ImageUrl  string
-	ReplyToID string
+	Images    []TweetImage
+	ReplyToID *int
 }
 
 type TweetRepository interface {
-	InsertTweet(ctx context.Context, model *Tweet) (*Tweet, error)
+	Insert(ctx context.Context, model *Tweet) (*Tweet, error)
 }
 
-func NewTweet(userId string, content string, ImageUrl string, ReplyToID string) (*Tweet, error) {
+func NewTweet(userId string, content string, ReplyToID string) (*Tweet, error) {
 	// content のドメインルール
 	if utf8.RuneCountInString(content) < contentLengthMin || utf8.RuneCountInString(content) > contentLengthMax {
 		return nil, apperrors.ReqBadParam.Wrap(apperrors.ErrMismatchData, fmt.Sprintf("content must be between %s and %s characters", contentLengthMin, contentLengthMax))
 	}
-	// ImageUrl のドメインルール_
-	if utf8.RuneCountInString(ImageUrl) < imageUrlLengthMin || utf8.RuneCountInString(ImageUrl) > imageUrlLengthMax {
-		return nil, apperrors.ReqBadParam.Wrap(apperrors.ErrMismatchData, fmt.Sprintf("imageUrl must be between %s and %s characters", imageUrlLengthMin, imageUrlLengthMax))
+	// ReplyToID
+	replyId, err := strconv.Atoi(ReplyToID)
+	if err != nil {
+		return nil, apperrors.ReqBadParam.Wrap(apperrors.ErrMismatchData, fmt.Sprintf("invalid reply_to_id: %v", ReplyToID))
 	}
 	return &Tweet{
-		ID:        ulid.Make().String(),
 		UserID:    userId,
 		Content:   content,
-		ImageUrl:  ImageUrl,
-		ReplyToID: ReplyToID,
-	}, nil
-}
-
-func ReconstructTweet(id string, userId string, content string, ImageUrl string, ReplyToID string) (*Tweet, error) {
-	// userIDの存在チェック
-
-	// content のドメインルール
-	if utf8.RuneCountInString(content) < contentLengthMin || utf8.RuneCountInString(content) > contentLengthMax {
-		return nil, apperrors.ReqBadParam.Wrap(apperrors.ErrMismatchData, fmt.Sprintf("content must be between %s and %s characters", contentLengthMin, contentLengthMax))
-	}
-	// ImageUrl のドメインルール_
-	if utf8.RuneCountInString(ImageUrl) < imageUrlLengthMin || utf8.RuneCountInString(ImageUrl) > imageUrlLengthMax {
-		return nil, apperrors.ReqBadParam.Wrap(apperrors.ErrMismatchData, fmt.Sprintf("imageUrl must be between %s and %s characters", imageUrlLengthMin, imageUrlLengthMax))
-	}
-	// ReplyToIDの存在チェック
-
-	return &Tweet{
-		ID:        id,
-		UserID:    userId,
-		Content:   content,
-		ImageUrl:  ImageUrl,
-		ReplyToID: ReplyToID,
+		ReplyToID: &replyId,
 	}, nil
 }
 
